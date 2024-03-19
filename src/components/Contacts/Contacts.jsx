700c4529b5d98abe635d75a15a99658e748fe490
Bruno Gomes <bsgomes16@gmail.com>
2024-03-18 22:28:13 -0300

    footer was almost concluded

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,9 +1,33 @@
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
+import { MdMarkUnreadChatAlt } from "react-icons/md";
+import { FaLinkedin, FaGithub, FaWhatsapp } from "react-icons/fa";
 
 export const Contacts = (props) => {
   return (
-    <section  id={props.id} className={styles.container}>
-      <h1>Contacts</h1>
-    </section>
-  )
-}
+    <footer id={props.id} className={styles.container}>
+      <div className={styles.title}>
+        {/* <h1>CONTACTS </h1> */}
+
+        <div className={styles.iconTitle}>
+          <MdMarkUnreadChatAlt />
+        </div>
+      </div>
+
+      <div className={styles.contentWrapper}>
+        <h1>LET'S WORK TOGETHER</h1>
+
+        <div className={styles.socialLinks}>
+          <a href="http://" target="_blank">
+            <FaLinkedin />
+          </a>
+          <a href="http://" target="_blank">
+            <FaGithub />
+          </a>
+          <a href="http://" target="_blank">
+            <FaWhatsapp />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
